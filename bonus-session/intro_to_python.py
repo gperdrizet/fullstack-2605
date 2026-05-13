349df62f48831f1483895b0818979fc215134e9a
George Perdrizet <george@perdrizet.org>
2026-05-12 22:29:26 -0400

    Added script and notebook examples

---
+++ bonus-session/intro_to_python.py
@@ -0,0 +1,25 @@
+'''
+Bonus session live coding file: Python basics
+
+Goals:
+- Show how to run Python code in a .py file (module or script)
+- Show that Python executes code top to bottom
+- Show the difference between syntax error and runtime error
+- Think of coding as an edit -> run -> read output -> fix it
+
+Run this file from a terminal:
+python intro_to_python.py
+'''
+
+# These statements are run in order
+print('Program started.')
+print('Currently running top-to-bottom.')
+
+# Uncomment line 18 for a syntax error
+# print('This line contains a syntax error)
+
+# Stores the string George to the variable user_name
+user_name = 'George'
+
+# Prints value of user name
+print(user_name)
