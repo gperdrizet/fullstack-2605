c90f88b35befeb966dce1b0bfafeb38766371595
George Perdrizet <george@perdrizet.org>
2026-05-18 00:47:34 -0400

    Use event delegation for download links

--- docs/javascripts/download.js
+++ docs/javascripts/download.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('a[href*="raw.githubusercontent.com"]').forEach(function (link) {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      var url = this.href;
-      var filename = url.split('/').pop();
-      fetch(url)
-        .then(function (response) { return response.blob(); })
-        .then(function (blob) {
-          var blobUrl = URL.createObjectURL(blob);
-          var a = document.createElement('a');
-          a.href = blobUrl;
-          a.download = filename;
-          document.body.appendChild(a);
-          a.click();
-          document.body.removeChild(a);
-          URL.revokeObjectURL(blobUrl);
-        });
+document.addEventListener('click', function (e) {
+  var link = e.target.closest('a[href*="raw.githubusercontent.com"]');
+  if (!link) return;
+  e.preventDefault();
+  var url = link.href;
+  var filename = url.split('/').pop();
+  fetch(url)
+    .then(function (response) { return response.blob(); })
+    .then(function (blob) {
+      var blobUrl = URL.createObjectURL(blob);
+      var a = document.createElement('a');
+      a.href = blobUrl;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(blobUrl);
     });
-  });
 });
